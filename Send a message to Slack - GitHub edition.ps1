a0b94919b6a26dbcd9c0f08dc29f2b4a01c60356
Steve Miller <26733161+ometecuhtli2001@users.noreply.github.com>
2020-08-19 14:01:44 -0700

    Send Slack message using PowerShell

---
+++ Send a message to Slack - GitHub edition.ps1
@@ -0,0 +1,46 @@
+﻿# Send an alert to Slack using PowerShell
+
+# Returns:
+#   Success - ok
+#   Error - either an HTTP response code or -1 if one could not be extracted
+function Send-SlackMessage{
+    Param(
+    [string]$SlackChannelUri="",
+    [string]$ChannelName,
+    [string]$Message,
+    [string]$Emoji=":ghost:",
+    [string]$Username=""
+    )
+
+    $reply=""
+
+    $Body = @"
+    {
+        "channel": "$ChannelName",
+        "username": "$Username",
+        "text": "$Message",
+        "icon_emoji":"$Emoji"
+    }
+"@
+    try {
+        if($PSVersionTable.PSVersion.Major -lt 3){ 
+            $client = New-Object "System.Net.WebClient"
+            $reply = $client.UploadString($SlackChannelUri, $Body)
+        } else {
+            $reply=Invoke-RestMethod -uri $SlackChannelUri -Method Post -body $body -ContentType 'application/json'
+        } # if..else: PS version
+    }
+    catch {
+        $gotcode=$_ -match "The remote server returned an error: \((\d+)\)"
+        if($gotcode){
+            $reply=$Matches[1]
+        } else {
+            $reply=-1 # Generic error indicator
+        } # if..else: is there an HTTP code to extract?
+    }
+    return $reply
+} # Send-SlackMessage
+
+$now=get-date -Format "MM/dd/yyyy HH:mm:ss"
+Send-SlackMessage -SlackChannelUri "https://hooks.slack.com/services/foo/bar/baz" -ChannelName "#testing" -Message "The time is $now"
+
